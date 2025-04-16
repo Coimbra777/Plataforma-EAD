a75b3a77a0b30244fc7f45546e8a0bf6869e99be
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2025-04-16 20:50:06 -0300

    feat: adição de controller, service e repository para CRUD de cursos part-1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\CourseController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/courses', [CourseController::class, 'index']);
+
 
 Route::get('/greeting', function () {
     return 'Hello World';
